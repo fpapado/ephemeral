3cfdbada516a22fcba601d86a8c78eaf3637227d
fpapado <fgpapado@gmail.com>
2017-08-09 14:58:52 +0100

    Add entry creation with current time

--- src/Page/Entry.elm
+++ src/Page/Entry.elm
@@ -3,10 +3,11 @@ module Page.Entry exposing (Model, Msg, initNew, update, view)
 import Data.Entry as Entry exposing (Entry, EntryLocation)
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onInput)
+import Html.Events exposing (onInput, onSubmit)
 import Http
 import Request.Entry
 import Date exposing (Date)
+import Task
 
 
 -- MODEL --
@@ -55,11 +56,16 @@ update msg model =
     case msg of
         Save ->
             let
-                reqCmd =
-                    Request.Entry.create model
-                        |> Http.send CreateCompleted
+                reqTask addedAt =
+                    Request.Entry.create { model | addedAt = addedAt }
+                        |> Http.toTask
+
+                getTimeAndSave =
+                    Date.now
+                        |> Task.andThen reqTask
+                        |> Task.attempt CreateCompleted
             in
-                ( model, reqCmd )
+                ( model, getTimeAndSave )
 
         CreateCompleted (Ok article) ->
             model ! []
@@ -80,10 +86,10 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    Html.form [ class "pa4 black-80" ]
-        [ div [ class "measure" ]
+    Html.form [ class "pa4 black-80", onSubmit Save ]
+        [ fieldset [ class "measure ba b--transparent ph0 mh0" ]
             [ div []
-                [ label [ class "f6 b db mb2", for "word" ] [ text "Word " ]
+                [ label [ class "f6 b db mv2", for "word" ] [ text "Word " ]
                 , input
                     [ attribute "aria-describedby" "word-desc"
                     , class "input-reset ba b--black-20 pa2 mb2 db w-100"
@@ -108,6 +114,8 @@ view model =
                 , small [ class "f6 black-60 db mb2", id "translation-desc" ]
                     [ text "The translation for the word." ]
                 ]
+            , button [ class "f6 link dim ph3 pv2 mt3 mb2 dib white bg-dark-blue bw0" ]
+                [ text "Save" ]
             ]
         ]
 
