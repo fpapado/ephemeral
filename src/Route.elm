7ef8b220e2b6040fa70e925047e565e7e0ff9206
fpapado <fgpapado@gmail.com>
2017-09-05 15:35:37 +0300

    Add routing module

---
+++ src/Route.elm
@@ -0,0 +1,81 @@
+module Route exposing (Route(..), fromLocation, href, modifyUrl)
+
+import Html exposing (Attribute)
+import Html.Attributes as Attr
+import Navigation exposing (Location)
+import UrlParser as Url exposing ((</>), Parser, oneOf, parseHash, s, string)
+
+
+-- ROUTING --
+
+
+type Route
+    = Home
+    | Login
+    | Logout
+    | Settings
+    | NewEntry
+
+
+
+-- | EditEntry Entry.EntryId
+
+
+route : Parser (Route -> a) a
+route =
+    oneOf
+        [ Url.map Home (s "")
+        , Url.map Login (s "login")
+        , Url.map Logout (s "logout")
+        , Url.map Settings (s "settings")
+        , Url.map NewEntry (s "entry")
+        ]
+
+
+
+-- INTERNAL --
+
+
+routeToString : Route -> String
+routeToString page =
+    let
+        pieces =
+            case page of
+                Home ->
+                    []
+
+                Login ->
+                    [ "login" ]
+
+                Logout ->
+                    [ "logout" ]
+
+                Settings ->
+                    [ "settings" ]
+
+                NewEntry ->
+                    [ "entry" ]
+    in
+        "#/" ++ String.join "/" pieces
+
+
+
+-- PUBLIC HELPERS --
+
+
+href : Route -> Attribute msg
+href route =
+    Attr.href (routeToString route)
+
+
+modifyUrl : Route -> Cmd msg
+modifyUrl =
+    routeToString >> Navigation.modifyUrl
+
+
+fromLocation : Location -> Maybe Route
+fromLocation location =
+    if String.isEmpty location.hash then
+        Just Home
+    else
+        parseHash route location
